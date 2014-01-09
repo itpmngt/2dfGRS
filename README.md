d4ecd0c277d20751f578d08c802572b995eeec9e
Joe Rosa <joe.rosa@itpmngt.co.uk>
2014-01-09 23:08:20 +0000

    Base on node instead of in browser
    Refactor functionality to make the files processing on Node and browser
    will focus on the canvas draw functionality

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-2dfGRS
-======
-
-Javascript interface to handle the 2df Galaxy Redshift Survey data.
--------------------------------------------------------------------
+# 2dfGRS
 
 This is a project to handle the [2dfGRS](http://www2.aao.gov.au/2dfgrs/) data using [NodeJS](http://nodejs.org/) server and [MongoDB](http://www.mongodb.org/) database engine to take advantage of the [Biga Data technologies](http://en.wikipedia.org/wiki/Big_data).
 
+It uses the NodeJS module FITS, and processes the data locally or through a browser.
 
-License:
---------
+
+## Usage:
+
+To read and get the FITS headers (HDU):
+
+	var fits = require('FITS');
+
+	fits.readFile(file, function(err, FITS){
+		if(err) return console.error(err);
+		console.log(FITS.HDU.primary);
+	});
+
+
+
+
+## License:
 The MIT License (MIT)
 
 Copyright � 2014 Joe Rosa
--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,12 @@
 // ### Function: main()
 function main(){
-
   // #### Configuration server URLs Routing
   F.server = 'api/file';
 
 	$.post(F.server, function(data) {
 
-    // FITS files are made in blocks of 2880 bytes (36 x 80 characters)
-    // Read data and create an array of lines of 80 characters each
-    F.lines = [];
-    for (var i=0; i<(data.length/80); i++) { F.lines.push(data.substring(i*80, (i*80)+80)) }
-
-    // Separate data and image blocks
-    F.FITS = { "filename": "File Name", "structures": [] };
-    F.headerFlag = true; F.headerBlock = []; F.dataBlock = [];
-
-    $.each(F.lines, function(idx, line){
-      if(!F.headerFlag){
-        if(line.substring(0,8)=='XTENSION'){
-          F.FITS.structures.push({ "header": F.headerBlock, "data": F.dataBlock});
-          // $('#viewFITS').append('<br /><br />');
-          F.headerFlag = true; F.headerBlock = []; F.dataBlock = [];}}
-      else{F.dataBlock.push(line)}
-      
-      if(F.headerFlag){
-        F.headerBlock.push(line);
-        // $('#viewFITS').append('<li>' + line + '</li>');
-        if(line.substring(0,8)=='END     '){F.headerFlag = false;} }
-    });
-
-    F.FITS.structures.push({ "header": F.headerBlock, "data": F.dataBlock});
-
-    $.each(F.FITS.structures, function(index, structure){
-      $.each(structure.header, function(idx, line){$('#viewFITS').append(line + '<br />')});
-      $('#viewFITS').append('<br /><br />');
-    });
+    F.fits = data;
+    $.each(F.fits.HDU.primary, function(key, value){ $('#viewFITS').append(key + ' = ' + value + '<br />')});
 
   });
 }
